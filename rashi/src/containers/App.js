96c3edbd3fba444898100e7285fc70cf5f409048
Rashi Abramson <roshkins@gmail.com>
2017-05-06 15:25:29 -0700

    add composer for php

--- rashi/src/containers/App.js
+++ rashi/src/containers/App.js
@@ -1,19 +1,24 @@
-import React, { Component, Text } from 'react';
-import PropTypes from 'prop-types';
+import React, { Component } from 'react';
+//import PropTypes from 'prop-types';
 import Webcam from 'react-webcam';
+import Camera from '../components/camera';
+import Login from '../components/login';
 import '../styles/App.css';
 
+var User = {
+  email: 'johndoe@example.org',
+  photos: ['//example.org/photos/photo.png','//example.org/photos/photo1.png'],
+  mostRecentPhoto: ['//example.org/photos/photo1.png'],
+  mostRecentBodyfat: 0.25,
+};
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Webcam className="Preview" />
-        <button>📸</button>
+        <Login />
         <br />
-        Or...
-        <br />
-        <input type="file" id="photoFile" />
+        <Camera />
       </div>
     );
   }
